0a8636f23a1fa5a194cf1eabc7cd56f5e95639bb
vgrigoriu <vgrigoriu@83898c08-7009-11de-9895-81829a6523ab>
2009-07-14 05:07:19 +0000

    Proof of concept: compile a file on the fly and run the resulting assembly.

---
+++ trunk/ProofOfConcept/CompileSomething/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Diagnostics;
+
+namespace CompileSomething
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            string exeName = Path.GetTempFileName() + ".exe";
+
+            string programToCompile = Templates.ClassWithMain.Replace("MAIN_BODY", this.sourceCodeTextBox.Text);
+
+            CompilerParameters parameters = new CompilerParameters();
+            parameters.GenerateExecutable = true;
+            parameters.GenerateInMemory = false;
+            parameters.IncludeDebugInformation = true;
+            parameters.OutputAssembly = exeName;
+            //parameters.ReferencedAssemblies.Add( "System.dll" );
+            parameters.TreatWarningsAsErrors = true;
+            //parameters.WarningLevel = 5;
+
+            CodeDomProvider provider = CodeDomProvider.CreateProvider("CSharp");
+            CompilerResults results = provider.CompileAssemblyFromSource(parameters, programToCompile);
+
+            errorsBox.Text = string.Empty;
+
+            if (results.Errors.Count > 0)
+            {
+                foreach (CompilerError error in results.Errors)
+                {
+                    errorsBox.Text += error.ToString() + Environment.NewLine;
+                }
+                //MessageBox.Show(results.Errors.Count.ToString() + " errors");
+            }
+            else
+            {
+                // actually run it
+                Process process = new Process();
+                process.StartInfo.FileName = exeName;
+                process.Start();
+                process.WaitForExit();
+            }
+
+            // TODO: clean up temp file?
+        }
+    }
+}
---
+++ trunk/ProofOfConcept/CompileSomething/Resources/ClassWithMain.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        MAIN_BODY
+    }
+}
